7df2f65e3b3197d934dd914d73422dd4dbc5ad2e
Kakoi1 <lopezrolandshane@gmail.com>
2024-09-28 09:42:10 +0800

    yawa

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\todosController;
+
+Route::view('/', 'welcome');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('/', [todosController::class, 'index'])->name("todo.home");
+
+    //create todo route
+    Route::get('/create', function () {
+        return view('create');
+    })->name("todo.create");
+
+    //edit todo route
+    Route::get('/edit/{id}', [todosController::class, 'edit'])->name("todo.edit");
+
+    //update todo route
+    Route::post('/update', [todosController::class, 'updateData'])->name("todo.updateData");
+
+    //store todo route
+    Route::post('/create', [todosController::class, 'store'])->name("todo.store");
+
+    //delete toto route
+    Route::get('/delete/{id}', [todosController::class, 'delete'])->name("todo.delete");
+});
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
